3ac37163586dfbdaf49ab294c49ba32ca5b7bf09
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2025-07-30 19:44:03 +0200

    Remove unused configs, update examples folder structure, update tests.

--- examples/stepik/stepik_classification.py
+++ examples/stepik/stepik_classification.py
@@ -5,8 +5,10 @@ from sklearn.datasets import make_classification
 
 from src.decision_tree_clf import MyTreeClf
 
+DATA_PATH = r'examples/stepik/data_banknote_authentication.txt'
+
 def read_data_classification() -> Tuple[pd.DataFrame, pd.Series]:
-    df = pd.read_csv('examples/stepik/data_banknote_authentication.txt', header=None)
+    df = pd.read_csv(DATA_PATH, header=None)
     df.columns = ['variance', 'skewness', 'curtosis', 'entropy', 'target']
     X, y = df.iloc[:,:4], df['target']
     
@@ -19,4 +21,5 @@ def stepik_classification(example_config: Dict[str, int]):
     model.fit(X, y)
     
     print(model.leafs_cnt)
-    model.build_tree()
+    leafs_sum = model.build_tree()
+    print(leafs_sum)
--- tests/test_decision_tree_classification.py
+++ tests/test_decision_tree_classification.py
@@ -6,12 +6,21 @@ from src.decision_tree_clf import MyTreeClf
 from helpers.config_parser import read_config
 
 CONFIGS: List[str]  = [
-    r'examples/stepik/tree1.config', 
-    r'examples/stepik/tree2.config', 
-    r'examples/stepik/tree3.config', 
-    r'examples/stepik/tree4.config', 
-    r'examples/stepik/tree5.config', 
-    r'examples/stepik/tree6.config',
+    r'examples/stepik/configs/tree1.config', 
+    r'examples/stepik/configs/tree2.config', 
+    r'examples/stepik/configs/tree3.config', 
+    r'examples/stepik/configs/tree4.config', 
+    r'examples/stepik/configs/tree5.config', 
+    r'examples/stepik/configs/tree6.config',
+]
+
+CONFIGS_BINS: List[str]  = [
+    r'examples/stepik/configs_bins/tree1.config', 
+    r'examples/stepik/configs_bins/tree2.config', 
+    r'examples/stepik/configs_bins/tree3.config', 
+    r'examples/stepik/configs_bins/tree4.config', 
+    r'examples/stepik/configs_bins/tree5.config', 
+    r'examples/stepik/configs_bins/tree6.config',
 ]
 
 LEAF_CNT: List[int] = [
@@ -23,7 +32,25 @@ LEAF_CNT: List[int] = [
     21,
 ]
 
-def test_stepik_classification():
+LEAF_SUM: List[int] = [
+    0.918956,
+    2.916956,
+    4.796617,
+    5.969142,
+    6.604379,
+    7.82549,
+]
+
+LEAF_SUM_BINS: List[int] = [
+    0.71033,
+    2.916956,
+    5.020575,
+    5.85783,
+    9.526468,
+    12.025427,
+]
+
+def test_stepik_classification_leafs_cnt():
     X, y = read_data_classification()
     
     for ind, config_path in enumerate(CONFIGS):
@@ -32,5 +59,20 @@ def test_stepik_classification():
         model.fit(X, y)
         assert model.leafs_cnt == LEAF_CNT[ind]
 
-
-
+def test_stepik_classification_leafs_sum():
+    X, y = read_data_classification()
+    
+    for ind, config_path in enumerate(CONFIGS):
+        config = read_config(config_path)
+        model = MyTreeClf(**config)
+        model.fit(X, y)
+        assert round(model.build_tree(), 6) == LEAF_SUM[ind]
+        
+def test_stepik_classification_leafs_sum_bins():
+    X, y = read_data_classification()
+    
+    for ind, config_path in enumerate(CONFIGS_BINS):
+        config = read_config(config_path)
+        model = MyTreeClf(**config)
+        model.fit(X, y)
+        assert round(model.build_tree(), 6) == LEAF_SUM_BINS[ind]
